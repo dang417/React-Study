7cdca3cf5203e18fe497200f2cda807e92e83bc7
신종혁 <shinjh4903@gmail.com>
2023-06-18 20:59:55 +0900

    6월 둘째주 ShinJongHyuk

---
+++ ShinJongHyuk/webapp/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Article from './components/Article';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Nav/>
+      <Article/>
+    </div>
+  );
+}
+
+export default App;
